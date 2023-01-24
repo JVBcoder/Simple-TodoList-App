b37ede66b5c9306c07851120ca6d8c3944b9a105
JVB <javivargasbreval@gmail.com>
2023-01-24 11:55:24 +0100

    Añadido la opción de editar tareas Mejorado el comportamiento de los formularios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import Filtro from './components/filtro';
+import Formulario from './components/formulario';
+import Listatareas from './components/listatareas';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de tareas por hacer</h1>
+    
+      <Listatareas></Listatareas>
+      <Filtro></Filtro>
+      <Formulario></Formulario>  
+     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Contexto2 from './components/contexto'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Contexto2>
     <App />
+  </Contexto2>
+
+  
   </React.StrictMode>
 );
 
